38a3adeedebf22104481f23edcad18a21e78a5e5
Samarth-Gupta30 <2k24.cs1n.2413899@gmail.com>
2026-08-17 12:38:34 +0530

    Configure GitHub Pages deployment pipeline

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/AI-PREP.IO/', // 👈 Make sure this matches your GitHub repo name exactly
   plugins: [
     tailwindcss(),
   ],
